6e318418dce07cd1688089159d5a60fc5f1eff8b
Mati <matias.abarca2018@gmail.com>
2021-04-15 16:32:57 -0400

    Ok se puede borrar de los dos arrays.

--- js/app.js
+++ js/app.js
@@ -1,32 +1,77 @@
 const botones = document.querySelectorAll('button');
+const input = document.querySelector('#input');
 
 let valorClick;
-const valores = [];
+let valores = [];
+let valores2 = [];
+let bandera = true;
 //Agregar los eventListeners
 botones.forEach( btn => {
     
     btn.addEventListener('click', e => {
         
-        valorClick = e.target.value;
+        if( isNaN( e.target.value ) ) valorClick = e.target.value;
+        else valorClick = parseInt( e.target.value);
 
         evaluarClick();
 
         console.log(valores);
+        console.log(valores2);
     });
 });
 
-function agregarClick(){
+function agregarClick( array ){
 
-    valores.push( valorClick );
+    array.push( valorClick );
 };
 
-function borrarClick(){
+function borrarClick( bandera ){
 
-    valores.pop( valores.length - 1); //El ultimo elemento se borrara.
+    let bandera1 = bandera;
+
+    if( valores2.length == 0 ){  
+    bandera1 = true;
+    console.log(bandera1);
+    };
+
+    if( bandera1 )valores.pop( valores.length - 1); //El ultimo elemento se borrara.
+    else valores2.pop( valores.length - 1);       
+        
 };
+function borrarTodo(){
+
+    valores = [];
+    valores2 = [];
+};
+
 function evaluarClick(){
 
-    if( valorClick !== 'atras' &&  valorClick !== 'borrar' && valorClick !== 'resultado') agregarClick();
+    if( valorClick !== 'atras' &&  valorClick !== 'borrar' && valorClick !== 'resultado' && valorClick !== '+'){
 
-    if( valorClick === 'atras' ) borrarClick();
+        if(bandera) agregarClick(  valores );
+        else agregarClick( valores2 );
+    };
+
+    if( valorClick === 'atras' ) borrarClick( bandera );
+
+    if( valorClick === 'borrar' ) borrarTodo();
+
+    if( valorClick === '+' || valorClick === '-' || valorClick === '*' || valorClick === '/' ) bandera = false ; console.log(bandera);
+
+    if( valorClick === 'resultado'){
+        
+        calcularResultado();
+        bandera = true;
+    } 
+};
+
+function calcularResultado(){ //Hasta ahora tengo funcionando suma.
+
+    const reduce = valores.reduce( (acc, valor )=> acc + valor);
+    const reduce2 = valores2.reduce( (acc, valor )=> acc + valor);
+
+    const suma = reduce + reduce2;
+    valores.length = 0;
+    valores2.length = 0;
+    input.value = suma.toString();
 };
